e3520692ca62415c63b3c9f0642beafaaf9cd793
Yttehs <shetty.yttehs@outlook.com>
2025-06-25 21:24:57 +0800

    feat: impl two areas

--- components/screens/editor/EditorScreen.tsx
+++ components/screens/editor/EditorScreen.tsx
@@ -1,5 +1,6 @@
 import BaseScreen from "@/components/base/BaseScreen";
-import { MD3Theme } from "react-native-paper";
+import { StyleSheet, View } from "react-native";
+import { MD3Theme, Text } from "react-native-paper";
 
 type Props = {
   theme: MD3Theme;
@@ -7,8 +8,27 @@ type Props = {
 
 export default function EditorScreen({ theme }: Props) {
   return (
-    <BaseScreen theme={theme}>
-      <></>
+    <BaseScreen theme={theme} style={styles.container}>
+      <View style={styles.leftArea}>
+        <Text>Left</Text>
+      </View>
+      <View style={styles.rightArea}>
+        <Text>Right</Text>
+      </View>
     </BaseScreen>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flexDirection: "row",
+    gap: 25,
+    padding: 25,
+  },
+  leftArea: {
+    flex: 1,
+  },
+  rightArea: {
+    flex: 1,
+  },
+});
